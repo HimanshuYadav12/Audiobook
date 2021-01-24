476e064bf09f1add6e45222f428752af533df91a
Himanshu yadav <76899541+HimanshuYadav12@users.noreply.github.com>
2021-01-24 17:18:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 
 speaker = pyttsx3.init()
-for num in range(6, pages):  # write the pageno. from where you have to star
+for num in range(6, pages):  # write the pageno. from where you have to start
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
